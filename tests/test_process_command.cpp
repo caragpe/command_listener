b74a7c7b6c6412ee17b993d4f769c40e30e502c1
Carlos Agullo <carlos.agullo@gmail.com>
2025-09-10 01:36:30 +0200

    fix: improves testing - using correct pointers, converting and freeing resources

--- tests/test_process_command.cpp
+++ tests/test_process_command.cpp
@@ -1,14 +1,18 @@
 #include "../include/process_command.h"
 #include <iostream>
-#include <cassert> // For assertions
+#include <cassert>
 
 int main()
 {
-  std::string result = process_command("hello");
+  char *raw_result = process_command("hello");
+  std::string result(raw_result);
+  free(raw_result);
   assert(result == "ACK: hello"); // Test passes if true
   std::cout << "Test passed: " << result << std::endl;
 
-  result = process_command("");
+  raw_result = process_command("");
+  result = std::string(raw_result);
+  free(raw_result);
   assert(result == "ACK: (null or empty command)");
   std::cout << "Empty test passed." << std::endl;
 
